c8c4d2977375dc9cfcf4c9ed7759bdf01ed1da7a
kastourik12 <kastouriw@yahoo.Fr>
2022-02-16 00:24:48 +0100

    final

--- src/app/components/display-shows/display-shows.component.ts
+++ src/app/components/display-shows/display-shows.component.ts
@@ -18,25 +18,41 @@ const shows : tvShow[] =[
   styleUrls: ['./display-shows.component.scss']
 })
 export class DisplayShowsComponent implements OnInit {
-  tvShows = shows;
+  tvShows!: tvShow[];
   keyword = '';
   sortType= '';
   constructor() {
 
+
   }
 
   ngOnInit(): void {
+    this.tvShows = shows;
   }
 
 
   addFavorite(show : tvShow ) {
     show.favorite=!show.favorite;
   }
-  sortData(){
-    if(this.sortType == '')
-    {
+  sortShows() {
 
+    if (this.sortType == 'Year ascending') {
+      this.tvShows.sort((a, b) => a.year > b.year ? 1 : -1)
     }
-
+    if (this.sortType == 'Year descending') {
+      this.tvShows.sort((a, b) => a.year < b.year ? 1 : -1)
+    }
+    if(this.sortType == 'Favorites'){
+      this.tvShows.sort((a )=> a.favorite ? 0:1)
+    }
+    if(this.sortType == 'Title ascending'){
+      this.tvShows.sort((a, b) => a.title > b.title ? 1 : -1)
+    }
+    if(this.sortType == 'Title descending'){
+      this.tvShows.sort((a, b) => a.title < b.title ? 1 : -1)
+    }
   }
+
+
+
 }
